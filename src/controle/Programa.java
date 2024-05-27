be5d50978df0d342ae9488279aa06b10fef2b005
Larissa Paiva <64439831+larissapaiva@users.noreply.github.com>
2024-05-27 15:17:26 -0300

    Add files via upload

---
+++ src/controle/Programa.java
@@ -0,0 +1,11 @@
+package controle;
+
+import view.JanelaConsulta;
+
+public class Programa {
+	public static void main(String[] args) {
+		JanelaConsulta js = new JanelaConsulta();
+
+		js.setVisible(true);
+	}
+}
